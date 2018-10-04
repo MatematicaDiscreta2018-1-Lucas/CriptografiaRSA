a816d88643a890a3e0a3ca37c09d57cd82706201
MateusBMP <mbmp@ic.ufal.br>
2018-10-04 11:25:40 -0300

    Escrita do calculo de d

--- chaves.c
+++ chaves.c
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include "chaves.h"
 
+l_int mod(l_int a, l_int b) {
+	l_int r = a % b;
+	if ((r < 0) && (b > 0)) return (b + r);
+	if ((r > 0) && (b < 0)) return (b + r);
+	return (r);
+}
+
 l_int elevar(l_int numero, l_int base, l_int potencia) {
 	if(potencia == 1) return numero; 
 	else return elevar(numero*base, base, potencia-1);
@@ -19,9 +26,10 @@ void escreveChave(l_int n, l_int e) {
 l_int geraN(l_int p, l_int q) {
 	return p * q;
 }
-
-l_int geraD(l_int p, l_int q, l_int e) {
-	int d;
 	
-	return ((p - 1) * (q - 1)) / (e + (e % ((p - 1) * (q - 1))));
+l_int geraD(l_int fiN, l_int e, int c) {
+	l_int r;
+	r = mod(e, fiN);
+    if (r == 0) return (mod((c / fiN), (e / fiN)));
+    return ((geraD(r, fiN, -c) * e + c) / (mod(fiN, e)));
 }
