44a90cc97fbff376b4b03346f457943895564118
MateusBMP <mbmp@ic.ufal.br>
2018-10-09 11:18:35 -0300

    Correcao de funcao para calcular D e adicao de nova funcao

--- chaves.c
+++ chaves.c
@@ -3,19 +3,6 @@
 #include <stdlib.h>
 #include "chaves.h"
 
-l_int mdc(l_int  a, l_int b, l_int *x, l_int *y) {
-	l_int xx, yy, d;
-	if (b == 0) {
-		*x = 1; 
-		*y = 0;
-		return a;
-	}
-	d = mdc(b, a%b, &xx, &yy);
-	*x = yy;
-	*y = xx - (a / b * yy);
-	return d;
-}
-
 void escreveChave(l_int n, l_int e) {
 	FILE *chave = NULL;
 	char nomeDoArquivo[FILENAME_MAX] = "ChavePublica.key";
@@ -29,8 +16,12 @@ l_int geraN(l_int p, l_int q) {
 }
 
 l_int geraD(l_int fiN, l_int e) {
-	l_int p, q, d;
-	d = mdc(e, fiN, &p, &q);
-  	if (p < 0) p = p + fiN;
-	return a;
+	l_int d;
+	for (d = 1; d <= fiN; d++) {
+		if ((e * d) % fiN == 1) return d;
+	}
+}
+
+l_int geraFiN(l_int p, l_int q) {
+	return (p - 1) * (q - 1);
 }
--- chaves.h
+++ chaves.h
@@ -1,6 +1,6 @@
 typedef unsigned long int l_int;
 
-l_int mdc(l_int  a, l_int b, l_int *x, l_int *y);		//Calcula mdc de a e b
 void escreveChave(l_int n, l_int e);		//Escreve a chave publica em um arquivo de texto
 l_int geraN(l_int p, l_int q);				//Calcula valor de n
-l_int geraD(l_int fiN, l_int e);		//Calcula valor de d
+l_int geraD(l_int fiN, l_int e);			//Calcula valor de d
+l_int geraFiN(l_int p, l_int q);			//Calcula valor de fiN
